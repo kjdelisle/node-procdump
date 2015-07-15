9f55f144482ee09a95b9ff49c472251544b668b5
Kevin Delisle <kjdelisle@gmail.com>
2015-07-15 11:22:09 -0400

    dummyProcess.js now spins off notepad as a separate top-level process

--- dummyProcess.js
+++ dummyProcess.js
@@ -2,8 +2,5 @@
 const fs = require('fs'),
 spawn = require('child_process').spawn;
 
-var dummy = spawn('notepad', []);
-
-dummy.on('close', function(){
-	console.dir("Oh noes! I have been closed!");
-});
+spawn('notepad', [], {"detached":true});
+process.exit();
